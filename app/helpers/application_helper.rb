cc85e4871b2cc7c43f57adf0ac660b46a30199df
Kyle Cline <kyledcline@gmail.com>
2015-04-27 17:40:26 -0400

    Add event date and linkable cards

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
       @title = "#{value} | TicketWin"
     end
   end
+
+  def weekday_with_time(datetime)
+    datetime.to_s(:weekday_with_time)
+      .gsub(/0\d/) { |n| n[1] }
+      .gsub(/AM|PM/, 'AM' => 'am', 'PM' => 'pm')
+  end
 end
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,1 @@
+Time::DATE_FORMATS[:weekday_with_time] = "%A %b %d, %I%p"
