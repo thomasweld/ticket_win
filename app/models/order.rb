4e9348709a364b2ad67ea117f22a60aae515c8df
Kyle Cline <kyledcline@gmail.com>
2015-06-04 10:06:16 -0500

    Annotate order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,10 +2,13 @@
 #
 # Table name: orders
 #
-#  id         :integer          not null, primary key
-#  user_id    :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  user_id         :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  event_id        :integer
+#  redemption_code :string
+#  delivery_email  :string
 #
 
 class Order < ActiveRecord::Base
