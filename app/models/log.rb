14e42b7d2ca85419be214f985cdc09c03b4d3311
Kyle Cline <kyledcline@gmail.com>
2015-07-01 00:08:34 -0500

    Log verbosely

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,9 @@
 class Log < ActiveRecord::Base
+  after_create :log_verbosely
+
+  private
+
+  def log_verbosely
+    Rails.logger.info "*** NEW EVENT ON #{self.object}: #{self.attributes}\n"
+  end
 end
