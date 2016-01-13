c08ee25dd38cd3c04a8b43abf1e523137e9039a6
Kyle Cline <kyledcline@gmail.com>
2016-01-13 10:24:54 -0600

    Allow admins to view checkout page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -148,7 +148,7 @@ class OrdersController < ApplicationController
   end
 
   def safe_referrer
-    if Rails.env.production? && !(request.referrer =~ /tcktwn\.com/)
+    if !current_user.try(:admin?) || (Rails.env.production? && !(request.referrer =~ /tcktwn\.com/))
       redirect_to root_path and return
     end
   end
