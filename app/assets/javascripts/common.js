703bec79b277169f82dfe05f2fd0623b9f7664a6
Kyle Cline <kyledcline@gmail.com>
2016-07-01 10:45:26 -0500

    Readify button loading

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,9 +1,8 @@
 var ready = function() {
+  $('.loading').on('click', function() {
+    var $btn = $(this).button('loading');
+  });
 }
 
 $(document).ready(ready);
 $(document).on('page:load', ready);
-
-$('.loading').on('click', function() {
-  var $btn = $(this).button('loading');
-});
